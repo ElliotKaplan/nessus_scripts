665a829f2dfd8259afe3e80c4b4ad2560778aa84
Elliot Kaplan <elliot.kaplan@occamsec.com>
2022-03-14 12:07:45 -0400

    converts synscan to something eyewitness compatible

---
+++ synscan_to_eyewitness.py
@@ -0,0 +1,29 @@
+from itertools import chain
+
+from nessus_session import NessusSession, nessus_script_arg_parse
+
+def get_synscan(sess, scan_number):
+    resp = sess.get('/scans/{}/plugins/11219'.format(scan_number))
+    dat = resp.json()
+    out = list()
+    for output in dat['outputs']:
+        for port, arr in output['ports'].items():
+            port = int(port.split()[0])
+            if port == 21:
+                out += ['ftp://{}'.format(h['hostname']) for h in arr]
+            if port == 80:
+                out += ['http://{}'.format(h['hostname']) for h in arr]
+            if port == 443:
+                out += ['https://{}'.format(h['hostname']) for h in arr]
+            if port > 1000:
+                out += ['http://{}:{}'.format(h['hostname'], port) for h in arr]
+                out += ['https://{}:{}'.format(h['hostname'], port) for h in arr]
+    return out
+
+if __name__=='__main__':
+    clargs = nessus_script_arg_parse('convert a syn scan to a file compatable with Eyewitness')
+
+    sess = NessusSession(clargs.NessusHost, clargs.ApiKey, clargs.SecretKey)
+    scan = get_synscan(sess, clargs.scan_no)
+    print('\n'.join(scan))
+    
