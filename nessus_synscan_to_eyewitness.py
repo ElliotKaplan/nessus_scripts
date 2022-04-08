6d4898220e40230c2abc61580599c9faa86c2422
Elliot Kaplan <elliot.kaplan@occamsec.com>
2022-04-08 10:41:39 -0400

    split off the http and https host/ports using already collected data

--- nessus_synscan_to_eyewitness.py
+++ nessus_synscan_to_eyewitness.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
+from collections import defaultdict
+
 from nessus_session import NessusSession, nessus_script_arg_parse
 
 def get_synscan(sess):
-    resp = sess.get('/plugins/11219')
-    dat = resp.json()
-    out = list()
-    for output in dat['outputs']:
+    synscan = sess.get('/plugins/11219').json()
+    # organize the output by port
+    ports = defaultdict(set)
+    for output in synscan['outputs']:
         for port, arr in output['ports'].items():
             port = int(port.split()[0])
-            if port == 21:
-                out += ['ftp://{}'.format(h['hostname']) for h in arr]
-            if port == 80:
-                out += ['http://{}'.format(h['hostname']) for h in arr]
-            if port == 443:
-                out += ['https://{}'.format(h['hostname']) for h in arr]
-            if port > 1000:
-                out += ['http://{}:{}'.format(h['hostname'], port) for h in arr]
-                out += ['https://{}:{}'.format(h['hostname'], port) for h in arr]
+            ports[port].update({h['hostname'] for h in arr})
+    
+    # check the tls/ssl plugins to sort out encrypted ports
+    sslvers = sess.get('/plugins/56984').json()
+    sslports = defaultdict(set)
+    for output in sslvers['outputs']:
+        for port, arr in output['ports'].items():
+            port = int(port.split()[0])
+            sslports[port].update({h['hostname'] for h in arr})
+            
+    out = list()
+    for port, hosts in ports.items():
+        if port == 21:
+            out += ['ftp://{}'.format(h['hostname']) for h in arr]
+            continue
+        elif port == 80:
+            out += ['http://{}'.format(h['hostname']) for h in arr]
+            continue
+        elif port == 443:
+            out += ['https://{}'.format(h['hostname']) for h in arr]
+            continue
+        elif port == 3389:
+            out += ['rdp://{}'.format(h['hostname']) for h in arr]
+            continue
+        # drop any port under 1000 as probably not an http(s) port
+        elif port < 1000:
+            continue
+        enc = sslports[port]
+        unenc = hosts.difference(enc)
+        out += ['http://{}:{}'.format(h, port) for h in unenc]
+        out += ['https://{}:{}'.format(h, port) for h in enc]
+        
     return out
 
 if __name__=='__main__':
